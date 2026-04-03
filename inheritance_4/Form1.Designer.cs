e63f62c554d7ec635a321eb378db6701601ecd30
Kirill Verkhozin <kirill.verkhozin@example.com>
2026-04-03 21:21:36 +0800

    базовый класс + форма

---
+++ inheritance_4/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace inheritance_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRefill = new Button();
+            txtInfo = new RichTextBox();
+            btnGet = new Button();
+            txtOut = new RichTextBox();
+            SuspendLayout();
+            // 
+            // btnRefill
+            // 
+            btnRefill.Location = new Point(12, 12);
+            btnRefill.Name = "btnRefill";
+            btnRefill.Size = new Size(371, 41);
+            btnRefill.TabIndex = 0;
+            btnRefill.Text = "Перезаполнить";
+            btnRefill.UseVisualStyleBackColor = true;
+            // 
+            // txtInfo
+            // 
+            txtInfo.BorderStyle = BorderStyle.None;
+            txtInfo.Location = new Point(12, 59);
+            txtInfo.Name = "txtInfo";
+            txtInfo.ReadOnly = true;
+            txtInfo.Size = new Size(271, 46);
+            txtInfo.TabIndex = 1;
+            txtInfo.Text = "";
+            // 
+            // btnGet
+            // 
+            btnGet.Location = new Point(289, 59);
+            btnGet.Name = "btnGet";
+            btnGet.Size = new Size(94, 209);
+            btnGet.TabIndex = 2;
+            btnGet.Text = "Взять";
+            btnGet.UseVisualStyleBackColor = true;
+            // 
+            // txtOut
+            // 
+            txtOut.Location = new Point(12, 111);
+            txtOut.Name = "txtOut";
+            txtOut.ReadOnly = true;
+            txtOut.Size = new Size(271, 157);
+            txtOut.TabIndex = 3;
+            txtOut.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(396, 285);
+            Controls.Add(txtOut);
+            Controls.Add(btnGet);
+            Controls.Add(txtInfo);
+            Controls.Add(btnRefill);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnRefill;
+        private RichTextBox txtInfo;
+        private Button btnGet;
+        private RichTextBox txtOut;
+    }
+}
---
+++ inheritance_4/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_4
+{
+    internal class Movie
+    {
+        public int Rating;
+
+        public virtual String GetInfo()
+        {
+            var str = String.Format("\nРейтинг: {0}", this.Rating);
+            return str;
+        }
+    }
+}
